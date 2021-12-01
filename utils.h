1b0bfc0361076dcc7ad950c82a4bfda47930dce2
KleberPF <43550602+KleberPF@users.noreply.github.com>
2021-12-01 20:06:51 -0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef ADVENT_OF_CODE_2021_UTILS_H
+#define ADVENT_OF_CODE_2021_UTILS_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool readIntFromLine(char *buffer, int length, FILE *f, int *value);
+
+#endif //ADVENT_OF_CODE_2021_UTILS_H
