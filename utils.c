279dddb9a4bd0d6f5252315127b87e9d4030944a
KleberPF <43550602+KleberPF@users.noreply.github.com>
2021-12-02 11:36:22 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,10 +1,25 @@
 #include "utils.h"
 
-bool readIntFromLine(char *buffer, int length, FILE *f, int *value)
+bool readIntFromLine(char* buffer, int length, FILE* f, int* value)
 {
     if (fgets(buffer, length, f)) {
         return sscanf(buffer, "%d", value);
     } else {
         return false;
     }
+}
+
+int getFileLength(FILE* f)
+{
+    fseek(f, 0, SEEK_END);
+    int length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    return length;
+}
+
+void readFileToBuffer(char* buffer, int length, FILE* f)
+{
+    fread(buffer, 1, length, f);
+    buffer[length] = 0;
 }
