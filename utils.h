8778ef00d16fee89f0764cd669dfe1f29bc26b42
KleberPF <43550602+KleberPF@users.noreply.github.com>
2021-12-02 11:36:40 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -6,4 +6,8 @@
 
 bool readIntFromLine(char *buffer, int length, FILE *f, int *value);
 
+int getFileLength(FILE *f);
+
+void readFileToBuffer(char *buffer, int length, FILE *f);
+
 #endif //ADVENT_OF_CODE_2021_UTILS_H
